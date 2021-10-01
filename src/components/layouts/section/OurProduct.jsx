454fffced1f09a6ac1352cabe0ca961aa819c895
robogeek95 <azeezlukman95@gmail.com>
2021-10-01 10:30:09 +0100

    fixed product size

--- src/components/layouts/section/OurProduct.jsx
+++ src/components/layouts/section/OurProduct.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Image from '../../images/slug.png';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Image from "../../images/slug.png";
 
 const imageStyle = {
-  position: 'absolute',
-  width: '300px',
-  height: '250px',
-  left: '-70px',
-  top: '-70px',
-
+  position: "absolute",
+  width: "300px",
+  height: "250px",
+  left: "-70px",
+  top: "-70px",
 };
 
 const overlay = {
-  position: 'relative',
+  position: "relative",
 };
 
 const items = {
   // width: '263px',
-  height: '242px',
-  marginBottom: '1rem',
+  height: "242px",
+  marginBottom: "1rem",
 };
 
 const itemText = {
-  fontStyle: 'normal',
-  fontWeight: 'normal',
-  fontSize: '20px',
-  lineHeight: '138.77%',
-  textAlign: 'center',
-  color: '#fff',
-  paddingTop: '6rem',
-
+  fontStyle: "normal",
+  fontWeight: "normal",
+  fontSize: "20px",
+  lineHeight: "138.77%",
+  textAlign: "center",
+  color: "#fff",
+  paddingTop: "6rem",
 };
 
 const OurProduct = () => (
@@ -40,30 +38,28 @@ const OurProduct = () => (
         <h5 className="ourProduct">Our Product</h5>
         <div>
           <p className="product-text">
-            What does the unbanked population do with the cash they receive from traditional remittance?
-            They primarily pay bills and then purchase basic consumer staple products locally.
+            What does the unbanked population do with the cash they receive from
+            traditional remittance? They primarily pay bills and then purchase
+            basic consumer staple products locally.
           </p>
-          <p className="product-text">75% of all cash received from remittance is spent in 4 categories:</p>
+          <p className="product-text">
+            75% of all cash received from remittance is spent in 4 categories:
+          </p>
           <ul>
             <li className="product-text text-muted">
-              <span className="text-danger">*</span>
-              {' '}
-              Bill Payment (Utilities & Telecom)
+              <span className="text-danger">*</span> Bill Payment (Utilities &
+              Telecom)
             </li>
             <li className="product-text text-muted">
-              <span className="text-danger">*</span>
-              {' '}
-              Household (Groceries & Clothing)
+              <span className="text-danger">*</span> Household (Groceries &
+              Clothing)
             </li>
             <li className="product-text text-muted">
-              <span className="text-danger">*</span>
-              {' '}
-              Medical (Clinic & Pharmacy)
+              <span className="text-danger">*</span> Medical (Clinic & Pharmacy)
             </li>
             <li className="product-text text-muted">
-              <span className="text-danger">*</span>
-              {' '}
-              Invoice Settlement (School fees, house rents, hospital bills)
+              <span className="text-danger">*</span> Invoice Settlement (School
+              fees, house rents, hospital bills)
             </li>
           </ul>
         </div>
@@ -73,7 +69,12 @@ const OurProduct = () => (
         <div className="card card-body rounded border-0 mb-3">
           <h5 className="ourProduct">Send Money</h5>
           <div className="mb-3">
-            <p className="product-text">If you still want to perform the traditional remittance, we make that happen as well. You  can send money to over 32 African countries on our secured platform and you won&apos;t have to wait for days for the fulfilment .</p>
+            <p className="product-text">
+              If you still want to perform the traditional remittance, we make
+              that happen as well. You can send money to over 32 African
+              countries on our secured platform and you won&apos;t have to wait
+              for days for the fulfilment .
+            </p>
             <Link to="/products/billPayment" className="para-card">
               Send Money here
             </Link>
@@ -86,9 +87,8 @@ const OurProduct = () => (
             <div className="item3" style={items}>
               <h6 style={itemText}>
                 Bill Payment
-                <br />
-                {' '}
-                (Utilities & Telecom)
+                <br />{" "}
+                <small className="text-info">(Utilities & Telecom)</small>
               </h6>
             </div>
           </Link>
@@ -96,9 +96,8 @@ const OurProduct = () => (
             <div className="item1" style={items}>
               <h6 style={itemText}>
                 Household
-                <br />
-                {' '}
-                (Groceries & Clothing)
+                <br />{" "}
+                <small className="text-info">(Groceries & Clothing)</small>
               </h6>
             </div>
           </Link>
@@ -107,8 +106,7 @@ const OurProduct = () => (
               <h6 style={itemText}>
                 Medical
                 <br />
-                {' '}
-                (Clinic & Pharmacy)
+                <small className="text-info">(Clinic & Pharmacy)</small>
               </h6>
             </div>
           </Link>
@@ -118,8 +116,9 @@ const OurProduct = () => (
               <h6 style={itemText}>
                 Invoice Settlement
                 <br />
-                {' '}
-                (School fees, house rents, hospital bills)
+                <small className="text-info">
+                  (School fees, house rents, bills)
+                </small>
               </h6>
             </div>
           </Link>
