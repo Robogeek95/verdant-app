5f742906b70bc1f53aa3ac811543636d38c76663
robogeek95 <azeezlukman95@gmail.com>
2021-10-02 22:44:31 +0100

    implementing forget password

--- src/components/ForgetPassword.jsx
+++ src/components/ForgetPassword.jsx
@@ -11,7 +11,6 @@ import { login } from "../actions/userActions";
 
 const ForgetPassword = ({ location, history }) => {
   const [email, setEmail] = useState("");
-  
 
   const redirect = location.search ? location.search.split("=")[1] : "/";
 
@@ -33,76 +32,67 @@ const ForgetPassword = ({ location, history }) => {
   };
 
   return (
-    <div className="d-flex justify-content-center" >
-      <div className="row">
-        <div className="col">
-          <div className=" p-5 ">
-            <div className="d-flex  justify-content-center">
-              <div>
-                <img src={Forgetlogo} alt="Brand Logo" />
-              </div>
-             
+    <div className="row justify-content-center ">
+      <div className="col-4">
+        <div className=" p-5 ">
+          <div className="d-flex  justify-content-center">
+            <div>
+              <img src={Forgetlogo} alt="Brand Logo" />
             </div>
-            <div className="text-center text-dark my-5">
-              <p style={{ fontSize: "20px", fontWeight: "500" }}>
-              Enter the email associated with your account
-and we’ll send an email with instructions to
-reset your password.
-              </p>
-            </div>
-
-            {error && <Message variant="danger">{error}</Message>}
-            {loading && <Loader />}
-
-            <form onSubmit={submitHandler} className="form">
-              <div className="form-group">
-                <label htmlFor="email">Email Address</label>
-                <input
-                  className="email"
-                  type="email"
-                  id="email"
-                  placeholder="Email Address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              
-             
-              <div className="mb-5">
-                <input
-                  className="btn btn-primary btn-block btn-lg"
-                  type="submit"
-                  value="Send"
-                />
-                {/* <button type="submit" className="btn btn-primary btn-block btn-lg">Submit</button> */}
-              </div>
-
-             
-            </form>
-
-            <div className="mt-5" />
-            <div className="mt-5" />
-
-          
-
-            <p className="no-account text-center">
-              Remember Password?
-              {/* <LinkContainer to={redirect ? `/register?redirect=${redirect}` : "/signup"}>  */}
-              <LinkContainer to="/login">
-                <a>Login</a>
-              </LinkContainer>
-              {/* <a href="#">Create an account</a> */}
+          </div>
+          <div className="text-center text-dark my-5">
+            <p style={{ fontSize: "20px", fontWeight: "500" }}>
+              Enter the email associated with your account and we’ll send an
+              email with instructions to reset your password.
             </p>
           </div>
-        </div>
 
-        {/* <div className="col">
+          {error && <Message variant="danger">{error}</Message>}
+          {loading && <Loader />}
+
+          <form onSubmit={submitHandler} className="form">
+            <div className="form-group">
+              <label htmlFor="email">Email Address</label>
+              <input
+                className="email"
+                type="email"
+                id="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <input
+                className="btn btn-primary btn-block btn-lg"
+                type="submit"
+                value="Send"
+              />
+              {/* <button type="submit" className="btn btn-primary btn-block btn-lg">Submit</button> */}
+            </div>
+          </form>
+
+          <div className="mt-5" />
+          <div className="mt-5" />
+
+          <p className="no-account text-center">
+            Remember Password?
+            {/* <LinkContainer to={redirect ? `/register?redirect=${redirect}` : "/signup"}>  */}
+            <LinkContainer to="/login">
+              <a>Login</a>
+            </LinkContainer>
+            {/* <a href="#">Create an account</a> */}
+          </p>
+        </div>
+      </div>
+
+      {/* <div className="col">
           <div>
             <img src={loginBarner} alt="Login Barner" />
           </div>
         </div> */}
-      </div>
     </div>
   );
 };
